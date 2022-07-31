99105bbc020407f1944fa328fe8d1759b794bb3f
AYUSH KRISHNA CHOUDHARY <103287965+ayushchoudharyk@users.noreply.github.com>
2022-07-31 17:06:55 +0530

    logo add

--- scripts/common.js
+++ scripts/common.js
@@ -13,7 +13,7 @@ class MyHeader extends HTMLElement {
     this.innerHTML = `<a href="index.html" class="header-logo">
           <img
             class="logo-img"
-            src="../assests/images/logo.png"
+            src="./assests/images/logo.png"
             alt="logo" /></a>
                   <button
                     id="loginBtn"
